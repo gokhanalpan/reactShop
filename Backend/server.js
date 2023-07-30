c29a73a49158e05143a35b3dc58dbef37fd517ec
gokhan alpan <alpangokhan@gmail.com>
2023-07-30 10:46:09 +0300

    befor deploy

--- Backend/server.js
+++ Backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 //cookie-parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -38,6 +34,18 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirName = path.resolve();
 console.log(__dirName);
 app.use("/uploads", express.static(path.join(__dirName, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirName, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirName, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
